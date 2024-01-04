93b1e8c5fecb8ddb6019aef3ed68920f0e8996cf
Yuchen Jiang <cdpiano2400@gmail.com>
2024-01-04 18:15:02 +0800

    Get USDC page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      DeRamp
+    <main className="flex min-h-screen flex-col items-center justify-evenly px-12">
+
+      <div className="flex flex-col items-center justify-between">
+        <h1 className="text-5xl font-extrabold my-8">$0</h1>
+        <h3 className="text-xl font-bold">0.00 USDC</h3>
+      </div>
+
+      <div className="flex flex-row items-center justify-between">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl m-4">
+          $20
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl m-4">
+          $50
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl m-4">
+          $100
+        </button>
+      </div>
+
+      <button className="bg-purple-500 h-12 hover:bg-purple-700 text-white font-bold rounded-3xl min-w-full">
+        Next
+      </button>
     </main>
   )
 }
