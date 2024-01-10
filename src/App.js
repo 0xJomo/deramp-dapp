dc0b8e9c1b4ff21b4869f1c8d24019d4249cc144
Yuchen Jiang <cdpiano2400@gmail.com>
2024-01-10 16:01:49 +0800

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { useState } from "react";
+import OnRamp from './pages/OnRamp';
 import Home from './pages/Home';
 import Payment from './pages/Payment'
 import ReviewOrder from './pages/ReviewOrder'
 import LockOrder from './pages/LockOrder'
 import { UserContext } from './context/UserContext.tsx';
+// import { PrivyProvider } from '@privy-io/react-auth';
 
 function App() {
   const [user, setUser] = useState(null);
   const [amount, setAmount] = useState(null);
 
+  const handleLogin = (user) => {
+    console.log(`User ${user.id} logged in!`)
+  }
+
   return (
+    // <PrivyProvider
+    //   appId={process.env.REACT_APP_PRIVY_APP_ID}
+    //   onSuccess={handleLogin}
+    // >
     <BrowserRouter>
       <UserContext.Provider value={{ user, setUser, amount, setAmount }}>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/onramp' element={<OnRamp />} />
           <Route path='/payment' element={<Payment />} />
           <Route path='/review' element={<ReviewOrder />} />
           <Route path='/lock' element={<LockOrder />} />
         </Routes>
       </UserContext.Provider>
     </BrowserRouter>
+    // </ PrivyProvider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,60 +1,24 @@
-import React, { useState } from 'react';
-import { useUserContext } from '../context/UserContext.tsx';
-import { useNavigate } from 'react-router-dom';
+// import { usePrivy } from '@privy-io/react-auth';
 
 export default function Home() {
-  const { setAmount } = useUserContext()
-  const [text, setText] = useState(null);
-  const navigate = useNavigate();
+  // const { login, logout, ready, authenticated, user } = usePrivy();
 
-  const setAmountAndRedirect = (value) => {
-    setAmount(value);
-    localStorage.setItem("amount", value);
-    navigate("/payment")
-  }
-
-  const handleInputChangeAndAdjustWidth = (e) => {
-    const value = e.target.value;
-    if (value.length === 0) {
-      e.target.style.width = '2rem';
-    } else {
-      e.target.style.width = `${value.length}ch`;
-    }
-    setText(e.target.value);
-  };
+  // if (ready && authenticated) {
+  //   console.log(user)
+  // }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly px-12">
+      <h1>DeRamp</h1>
 
-      <div className="flex flex-col items-center justify-between">
-        <div className='flex flex-row items-center justify-center my-8'>
-          <h1>$</h1>
-          <input
-            type="text"
-            value={text}
-            className='text-5xl font-extrabold focus:outline-none w-8'
-            placeholder='0'
-            onChange={handleInputChangeAndAdjustWidth}
-          />
-        </div>
-        <h3 className="text-xl font-bold">{text || 0}.00 USDC</h3>
+      <div className="flex flex-col min-w-full">
+        <button className="bg-purple-500 h-12 hover:bg-purple-700 text-white font-bold rounded-3xl min-w-full">
+          Get Crypto
+        </button>
+        <p className="text-center mt-4">
+          Log out
+        </p>
       </div>
-
-      <div className="flex flex-row items-center justify-between">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl m-4" onClick={() => setAmountAndRedirect(20)}>
-          $20
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl m-4" onClick={() => setAmountAndRedirect(50)}>
-          $50
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl m-4" onClick={() => setAmountAndRedirect(100)}>
-          $100
-        </button>
-      </div>
-
-      <button className="bg-purple-500 h-12 hover:bg-purple-700 text-white font-bold rounded-3xl min-w-full" onClick={() => setAmountAndRedirect(parseInt(text))}>
-        Next
-      </button>
     </main >
   );
 }
